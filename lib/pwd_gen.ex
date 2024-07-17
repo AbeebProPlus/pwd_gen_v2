a3c97a2e0e8b1501e5fceddc0417c863f0685351
abeeb ahmad <mdahlat94@gmail.com>
2024-07-17 18:05:29 +0100

    password gen v2

---
+++ lib/pwd_gen.ex
@@ -0,0 +1,103 @@
+defmodule PwdGen do
+  @symbols "!#$%&()*+,-./:;<=>?@[]^_{|}~"
+
+  def generate(options) do
+    validate_length(options)
+  end
+
+  defp validate_length(options) do
+    has_length = Map.has_key?(options, "length")
+    message = "Provide a length"
+
+    if has_length === true do
+      validate_length_is_integer(options)
+    else
+      error(message)
+    end
+  end
+
+  defp validate_length_is_integer(options) do
+    number_arr = Enum.map(0..9, fn x -> Integer.to_string(x) end)
+    is_length_int = Enum.member?(number_arr, options["length"])
+
+    if is_length_int == true do
+      validate_options_are_boolean(options)
+    else
+      message = "Provide an integer"
+      error(message)
+    end
+  end
+
+  defp error(message) do
+    {:error, message}
+  end
+
+  defp validate_options_are_boolean(options) do
+    options_without_length = Map.delete(options, "length")
+    options_values = Map.values(options_without_length)
+
+    all_booleans =
+      Enum.all?(options_values, fn val -> val |> String.to_atom() |> is_boolean() end)
+
+    if all_booleans == true do
+      validate_options(options)
+    else
+      message = "Provide boolean data"
+      error(message)
+    end
+  end
+
+  defp validate_options(options) do
+    length_to_integer = options["length"] |> String.trim() |> String.to_integer()
+    options_without_length = Map.delete(options, "length")
+    new_arr = ["lowercase" | extracted_true_values(options_without_length)]
+    random_char_arr = Enum.map(new_arr, &get/1)
+    length = length_to_integer - length(random_char_arr)
+    random_strings = generate_password(length, options)
+    strings = random_char_arr ++ random_strings
+    invalid_option? = false in strings
+
+    case invalid_option? do
+      true ->
+        {:error, "Only options allowed numbers, uppercase, symbols."}
+
+      false ->
+        string =
+          strings
+          |> Enum.shuffle()
+          |> to_string()
+
+        {:ok, string}
+    end
+  end
+
+  defp extracted_true_values(options) do
+    Enum.reduce(options, [], fn {k, v}, acc ->
+      if v |> String.trim() |> String.to_existing_atom(), do: [k | acc], else: acc
+    end)
+  end
+
+  defp get("lowercase") do
+    <<Enum.random(?a..?z)>>
+  end
+
+  defp get("uppercase") do
+    <<Enum.random(?A..?Z)>>
+  end
+
+  defp get("numbers") do
+    <<Enum.random(0..9)>>
+  end
+
+  defp get("symbols") do
+    @symbols |> String.split("", trim: true) |> Enum.random()
+  end
+
+  defp get(_option) do
+    false
+  end
+
+  defp generate_password(length, options) do
+    Enum.map(1..length, fn _ -> options |> Enum.random() |> get() end)
+  end
+end
---
+++ test/pwd_gen_test.exs
@@ -0,0 +1,49 @@
+defmodule PwdGenTest do
+  use ExUnit.Case
+
+  describe "Password generator" do
+    setup do
+      parameters = %{
+        "length" => "10",
+        "numbers" => "false",
+        "uppercase" => "false",
+        "symbols" => "false"
+      }
+
+      {:ok, result} = PwdGen.generate(parameters)
+
+      parameters_type = %{
+        lowercase_arr: Enum.map(?a..?z, &<<&1>>),
+        digits: Enum.map(0..9, &Integer.to_string(&1)),
+        uppercase: Enum.map(?A..?Z, &<<&1>>),
+        symbols: String.split("!#$%&()*+,-./:;<=>?@[]^_{|}~", "", trim: true)
+      }
+
+      %{
+        parameters_type: parameters_type,
+        result: result
+      }
+    end
+  end
+
+  test "returns error when no length is provided" do
+    param = %{"invalid" => "false"}
+    assert {:error, "Provide a length"} = PwdGen.generate(param)
+  end
+
+  test "returns error if length is not integer" do
+    param = %{"length" => "ab"}
+    assert {:error, "Provide an integer"} = PwdGen.generate(param)
+  end
+
+  test "returns error with invalid data" do
+    param = %{
+      "length" => "1",
+      "numbers" => "fase",
+      "uppercase" => "false",
+      "symbols" => "fase"
+    }
+
+    assert {:error, "Provide boolean data"} = PwdGen.generate(param)
+  end
+end
